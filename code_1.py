4f0bc81b918b0611f6850558f3dde29f7dd3d9ca
RafaelLeone <rinaciodacosta@gmail.com>
2023-09-01 14:36:32 -0300

    added random options for fun

--- code_1.py
+++ code_1.py
@@ -1,5 +1,6 @@
 import pyautogui
 import time
+import random
 
 # Use the keyboard shortcut to open the terminal
 pyautogui.hotkey('ctrl', 'alt', 't')
@@ -37,15 +38,24 @@ button_dict = {'w': 'UP', 's': 'DOWN', 'a': 'LEFT', 'd': 'RIGHT',
                'l': 'A', 'k': 'B', 'i': 'L', 'o': 'R',
                'enter': 'START', 'backspace': 'SELECT'}
 
+random_behavior_list = [UP, DOWN, LEFT, RIGHT, A, B]
+
 def press_button(key, n_times = 1):
     for _ in range(n_times):
         pyautogui.keyDown(key)
+        if key == UP or key == DOWN or key == LEFT or key == RIGHT:
+            time.sleep(0.1)
         pyautogui.keyUp(key)
         print(button_dict[key])
         time.sleep(1)  
 
-def hold_button():
-    ...
-
 # Select File
 press_button(A, 4)
+
+# Random behavior
+i = 0
+while True:
+    action = random.choice(random_behavior_list)
+    press_button(action)
+    if i == 50:
+        break
